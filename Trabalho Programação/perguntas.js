c5536c46f8f1f1beb2868cb2378bf684f537d156
diego2121n <diego.souza03@escola.pr.gov.br>
2025-06-17 22:27:15 -0300

    Add files via upload

---
+++ Trabalho Programação/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Fato',
+    'Vergonha do Paraná',
+    'Coritiba'
+)
+
+criaCartao(
+    'Geografia',
+    'Localização do melhor estádio?',
+    'Arena da Baixada '
+)
+
+criaCartao(
+    'Neymar',
+    'Próximo time do rei?',
+    'Paraná FC 💀💀💀'
+)
+
+criaCartao(
+    'Verdade',
+    'Cara mais atleta do mundo?',
+    'Buiu'
+)
